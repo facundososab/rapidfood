6abfab9ccb01c792f6723f62552a0e7d7879c6b7
marchese_valentin <marchese52002@gmail.com>
2026-08-11 13:34:11 -0300

    feat(catalog): agrego los 5 casos de uso (CreateProduct, SetProductState, AddPrice, CreateCategory, SetDiscount)

---
+++ api/modules/catalog/application/use_cases/create_category_use_case.py
@@ -0,0 +1,33 @@
+from modules.catalog.application.ports.driver.category_ports import (
+    CreateCategoryCommand,
+    CreateCategoryPort,
+    CreateCategoryResponse,
+)
+from modules.catalog.application.ports.driven.category_repository_port import (
+    CategoryRepositoryPort,
+)
+from modules.catalog.application.ports.driven.id_generator_port import IdGeneratorPort
+from modules.catalog.domain.models.category import Category
+
+
+class CreateCategoryUseCase(CreateCategoryPort):
+    def __init__(
+            self,
+            categories: CategoryRepositoryPort,
+            id_generator: IdGeneratorPort,
+    ) -> None:
+        self._categories = categories
+        self._id_generator = id_generator
+
+    def execute(self, command: CreateCategoryCommand) -> CreateCategoryResponse:
+        category = Category(
+            id=self._id_generator.generate(),
+            description=command.description,
+        )
+        self._categories.save(category)
+
+        return CreateCategoryResponse(
+            id=category.id,
+            description=category.description,
+        )
+    
